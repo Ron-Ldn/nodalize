550f3733a36eeed70d0b7f6d9c397cfd8185b186
Ron-Ldn <ronan.mouquet@gmail.com>
2023-06-11 18:43:41 +0100

    Minor changes to help upload to pypi

--- setup.py
+++ setup.py
@@ -8,15 +8,11 @@ setup(
     version="1.0.0",
     author="Ronan Mouquet",
     author_email="ronan.mouquet@gmail.com",
-    description=("todo"),
+    description="Framework to organize data transformation flow.",
+    long_description_content_type='text/markdown',
     license="MIT",
     keywords="",
     url="https://github.com/Ron-Ldn/nodalize",
     packages=find_packages(exclude=['tests*', 'demos*']),
     long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
-    classifiers=[
-        "Development Status :: 4 - Production/Stable",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
-    ],
 )
